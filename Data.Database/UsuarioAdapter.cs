10fe2a5ad8a717beaf3dae5f8568512298546a63
Rinava <asdasd5056@gmail.com>
2019-09-02 17:51:04 -0300

    Adaptación de usuario a la base de datos

--- Data.Database/UsuarioAdapter.cs
+++ Data.Database/UsuarioAdapter.cs
@@ -98,7 +98,7 @@ namespace Data.Database
             {
                 this.OpenConnection();
                 SqlCommand cmdUsuarios = new SqlCommand("select * from Usuario where idUsuario @id", sqlConn);
-                cmdUsuarios
+                cmdUsuarios.Parameters.Add("@id", SqlDbType.Int).Value = ID;
                 SqlDataReader drUsuarios = cmdUsuarios.ExecuteReader();
                 while (drUsuarios.Read())
                 {
@@ -124,23 +124,92 @@ namespace Data.Database
 
         public void Delete(int ID)
         {
-            Usuarios.Remove(this.GetOne(ID));
+            try
+            {
+                //abrimos la conexion
+                this.OpenConnection();
+
+                //creamos la sentencia sql y asignamos un valor al parametro
+                SqlDbType cmdDelete =
+                    new SqlCommand("delete usuarios where idUsuario=@id",sqlConn);
+                cmdDelete.Parameters.Add("@id",SqlDbType.Int).Value= ID;
+
+                //ejecutamos la sentencia sql
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada=
+                    new Exception("Error al eliminar usuario", Ex);
+                trow ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+        protected void Update(Usuario usuario)
+        {
+            try
+            { 
+                this.OpenConnection();
+                SqlCommand cmdSave=new SqlCommand(
+                    "UPDATE usuario SET clave=@clave, "+ 
+                    "habilidato= @habilitado, nombre=@nombre, apellido=@apellido, email=@email"+
+                    "WHERE idUsuario=@id", sqlConn);
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = usuario.ID;
+                cmdSave.Parameters.Add("@clave", SqlDbType.VarChar, 50).Value = usuario.Clave;
+                cmdSave.Parameters.Add("@habilitado", SqlDbType.Bit).Value = usuario.Habilitado;
+                cmdSave.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = usuario.Nombre;
+                cmdSave.Parameters.Add("@apellido", SqlDbType.VarChar,50).Value=usuario.Apellido;
+                cmdSave.Parameters.Add("@email", SqlDbType.VarChar, 50).Value=usuario.Email;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Exception ExcepcionManejada =
+                    new Exception("Error al modificar datos del usuario", ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+        protected void Insert(Usuario usuario)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave= new SqlCommand(
+                   "insert into usuarios(clave,habilitado,nombre,apellido,email)"+
+                   "values (@clave,@habilitado,@nombre,@apellido,@email)"+
+                   "select @@identity", //esta linea es para recuperar el ID que asigno el sql automaticamente
+                   sqlConn);
+                cmdSave.Parameters.Add("@clave",SqlDbType.VarChar,50).Value=usuario.Clave;
+                cmdSave.Parameters.Add("@habilitado",SqlDbType.bit).Value=usuario.Habilitado;
+                cmdSave.Parameters.Add("@nombre",SqlDbType.VarChar,50).Value=usuario.Nombre;
+                cmdSave.Parameters.Add("@apellido",SqlDbType.VarChar,50).Value=usuario.Apellido;
+                cmdSave.Parameters.Add("@email",SqlDbType.VarChar,50).Value=usuario.Email;
+                usuario.ID=Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+                //asi se obtiene el ID que asigno al bd automaticamente
+            }
+            catch (Exception ex) 
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear usuario", ex);
+                throw ExcepcionManejada;
+            }
+            finally 
+            {
+                this.CloseConnection();
+            }
         }
 
         public void Save(Usuario usuario)
         {
             if (usuario.State == BusinessEntity.States.New)
             {
-                int NextID = 0;
-                foreach (Usuario usr in Usuarios)
-                {
-                    if (usr.ID > NextID)
-                    {
-                        NextID = usr.ID;
-                    }
-                }
-                usuario.ID = NextID + 1;
-                Usuarios.Add(usuario);
+                this.Insert(usuario);
             }
             else if (usuario.State == BusinessEntity.States.Deleted)
             {
@@ -148,7 +217,7 @@ namespace Data.Database
             }
             else if (usuario.State == BusinessEntity.States.Modified)
             {
-                Usuarios[Usuarios.FindIndex(delegate(Usuario u) { return u.ID == usuario.ID; })]=usuario;
+                this.Update(usuario);
             }
             usuario.State = BusinessEntity.States.Unmodified;            
         }
