361c89b981c5d634d8d7044a62930ba8446208ed
Gexygamma <gex98f@gmail.com>
2020-01-10 04:55:20 -0300

    agregar utilidad de validacion

---
+++ Util/Validacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Util
+{
+    public class Validacion
+    {
+        private const string EmailRegEx = ""; // TODO: Agregar RegEx para validación de emails.
+        private const int LongitudMinimaContraseña = 6;
+
+        public static bool ValidarEmail(string email)
+        {
+            return true; // Regex.IsMatch(email, EmailRegEx);
+        }
+
+        public static bool ValidarContraseña(string contraseña)
+        {
+            return contraseña.Length >= LongitudMinimaContraseña;
+        }
+    }
+}
