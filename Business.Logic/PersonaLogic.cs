2755bf897dfb07f86aeb9e35565691cf157cf148
Gexygamma <gex98f@gmail.com>
2020-01-12 02:19:42 -0300

    añadido adaptadores de persona a las capas de logica y datos

---
+++ Business.Logic/PersonaLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class PersonaLogic
+    {
+        private PersonaAdapter PersonaData { get; set; }
+
+        public PersonaLogic()
+        {
+            PersonaData = new PersonaAdapter();
+        }
+
+        public List<Persona> GetAll()
+        {
+            return PersonaData.GetAll();
+        }
+
+        public Persona GetOne(int ID)
+        {
+            return PersonaData.GetOne(ID);
+        }
+
+        public void Delete(int ID)
+        {
+            PersonaData.Delete(ID);
+        }
+
+        public void Save(Persona persona)
+        {
+            PersonaData.Save(persona);
+        }
+    }
+}
---
+++ Data.Database/PersonaAdapter.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using Business.Entities;
+
+namespace Data.Database
+{
+    public class PersonaAdapter : Adapter
+    {
+        private Persona CrearPersonaDesdeReader(SqlDataReader dr)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = (string)dr["nombre"];
+            persona.Apellido = (string)dr["apellido"];
+            persona.Direccion = (string)dr["direccion"];
+            persona.Email = (string)dr["email"];
+            persona.Telefono = (string)dr["telefono"];
+            persona.FechaNacimiento = (DateTime)dr["fecha_nac"];
+            persona.Legajo = (int)dr["legajo"];
+            persona.TipoPersona = (TipoPersona)dr["tipo_persona"];
+            return persona;
+        }
+
+        public List<Persona> GetAll()
+        {
+            List<Persona> personas = new List<Persona>();
+            try
+            {
+                OpenConnection();
+                SqlCommand cmdPersonas = new SqlCommand("select * from personas", sqlConn);
+                SqlDataReader drPersonas = cmdPersonas.ExecuteReader();
+                while (drPersonas.Read())
+                {
+                    Persona persona = CrearPersonaDesdeReader(drPersonas);
+                    personas.Add(persona);
+                }
+                drPersonas.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar lista de personas", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return personas;
+        }
+
+        public Persona GetOne(int ID)
+        {
+            Persona persona;
+            try
+            {
+                OpenConnection();
+                SqlCommand cmdPersona = new SqlCommand("select * from usuarios where id_usuario=@id", sqlConn);
+                cmdPersona.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drPersonas = cmdPersona.ExecuteReader();
+                if (drPersonas.Read())
+                {
+                    persona = CrearPersonaDesdeReader(drPersonas);
+                }
+                else
+                {
+                    persona = null;
+                }
+                drPersonas.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar persona por id", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return persona;
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                OpenConnection();
+                SqlCommand cmdDelete = new SqlCommand("delete personas where id_persona=@id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar persona", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        protected void Update(Persona persona)
+        {
+            try
+            {
+                OpenConnection();
+                SqlCommand cmdSave = new SqlCommand(
+                    "UPDATE usuarios SET nombre=@nombre, apellido=@apellido, direccion=@direccion, email=@email, " +
+                    "telefono=@telefono, fecha_nac=@fecha_nac, legajo=@legajo, tipo_persona=@tipo_persona " +
+                    "WHERE id_usuario=@id", sqlConn);
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = persona.ID;
+                cmdSave.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = persona.Nombre;
+                cmdSave.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = persona.Apellido;
+                cmdSave.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = persona.Direccion;
+                cmdSave.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = persona.Email;
+                cmdSave.Parameters.Add("@telefono", SqlDbType.VarChar, 50).Value = persona.Telefono;
+                cmdSave.Parameters.Add("@fecha_nac", SqlDbType.DateTime).Value = persona.FechaNacimiento;
+                cmdSave.Parameters.Add("@legajo", SqlDbType.Int).Value = persona.Legajo;
+                cmdSave.Parameters.Add("@tipo_persona", SqlDbType.Int).Value = (int)persona.TipoPersona;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al modificar datos de la persona", ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+        protected void Insert(Persona persona)
+        {
+            try
+            {
+                OpenConnection();
+                SqlCommand cmdSave = new SqlCommand(
+                   "insert into personas(nombre,apellido,direccion,email,telefono,fecha_nac,legajo,tipo_persona)" +
+                   "values (@nombre,@apellido,@direccion,@email,@telefono,@fecha_nac,@legajo,@tipo_persona)" +
+                   "select @@identity", // Esta última línea es para recuperar el ID autogenerado desde la bd.
+                   sqlConn);
+                cmdSave.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = persona.Nombre;
+                cmdSave.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = persona.Apellido;
+                cmdSave.Parameters.Add("@direccion", SqlDbType.VarChar, 50).Value = persona.Direccion;
+                cmdSave.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = persona.Email;
+                cmdSave.Parameters.Add("@telefono", SqlDbType.VarChar, 50).Value = persona.Telefono;
+                cmdSave.Parameters.Add("@fecha_nac", SqlDbType.DateTime).Value = persona.FechaNacimiento;
+                cmdSave.Parameters.Add("@legajo", SqlDbType.Int).Value = persona.Legajo;
+                cmdSave.Parameters.Add("@tipo_persona", SqlDbType.Int).Value = (int)persona.TipoPersona;
+                // Se obtiene el ID autogenerado y se lo guarda a la entidad.
+                persona.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al insertar persona", ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        public void Save(Persona persona)
+        {
+            if (persona.State == BusinessEntity.States.New)
+            {
+                Insert(persona);
+            }
+            else if (persona.State == BusinessEntity.States.Deleted)
+            {
+                Delete(persona.ID);
+            }
+            else if (persona.State == BusinessEntity.States.Modified)
+            {
+                Update(persona);
+            }
+            persona.State = BusinessEntity.States.Unmodified;
+        }
+    }
+}
