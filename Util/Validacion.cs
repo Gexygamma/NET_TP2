3708fee4b61f1312e436c13ea853a3d70a211cb2
Gexygamma <gex98f@gmail.com>
2020-02-08 16:59:18 -0300

    validacion de email implementada

--- Util/Validacion.cs
+++ Util/Validacion.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace Util
 {
     public class Validacion
     {
-        private const string EmailRegEx = ""; // TODO: Agregar RegEx para validación de emails.
         private const int LongitudMinimaContraseña = 6;
 
         public static bool ValidarEmail(string email)
         {
-            return true; // Regex.IsMatch(email, EmailRegEx);
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public static bool ValidarClave(string clave, string confirmarClave)
